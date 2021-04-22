a9429ea5819fbd0cc0cd48e15a914cf94587b574
ShaoBaoEr <32100008+ninthDevilHAUNSTER@users.noreply.github.com>
2021-04-22 11:41:53 +0800

    Update command.sh
    ADD database detail

--- AstGreatImporter/command.sh
+++ AstGreatImporter/command.sh
@@ -3,6 +3,7 @@
 #rm -rf ./neo4j-community-4.2.1/data/databases/neo4j/
 # your neo4j path
 ./neo4j-community-4.2.1/bin/neo4j-admin import \
+--database=neo4j \ 
 --nodes=nodes.csv \
 --relationships=rels.csv \
 --relationships=cpg_edges.csv \
